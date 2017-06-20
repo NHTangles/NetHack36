9ee39ec4e1f9eebaa2bf2216d3c3cecc7d97d64b
k21971 <keith.simpson1971@gmail.com>
2017-06-19 23:30:10 -0400

    receiving Excalibur livelog message knows hero's gender

--- src/fountain.c
+++ src/fountain.c
@@ -394,7 +394,7 @@ register struct obj *obj;
             obj->oeroded = obj->oeroded2 = 0;
             obj->oerodeproof = TRUE;
             exercise(A_WIS, TRUE);
-	    livelog_write_string(LL_ARTIFACT, "had Excalibur thrown at them by some watery tart"); /* Monty Python and the Holy Grail ;) */
+	    livelog_printf(LL_ARTIFACT, "had Excalibur thrown at %s by some watery tart",uhim()); /* Monty Python and the Holy Grail ;) */
         }
         update_inventory();
         levl[u.ux][u.uy].typ = ROOM;
