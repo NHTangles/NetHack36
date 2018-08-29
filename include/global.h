2b67612d35d00fe1afcdf3231cc8da33cdbe1663
k21971 <keith.simpson1971@gmail.com>
2018-08-29 09:41:24 -0400

    Livelog: Player murders their own pet (from xNetHack, git b746c23)

--- include/global.h
+++ include/global.h
@@ -360,10 +360,11 @@ struct savefile_info {
 #define LL_LIFESAVE   0x0010 /* Use up amulet of lifesaving */
 #define LL_CONDUCT    0x0020 /* Break conduct - not reported early-game */
 #define LL_ARTIFACT   0x0040 /* Excalibur, Sting, Orcrist, plus sac gifts and artwishes */
-#define LL_GENOCIDE   0x0080 /* Logging of genocides */ 
+#define LL_GENOCIDE   0x0080 /* Logging of genocides */
 #define LL_DUMP_ASC   0x0100 /* Log URL for dumplog if ascended */
 #define LL_DUMP_ALL   0x0200 /* Log dumplog url for all games */
-#define LL_DEBUG      0x8000 /* For debugging messages and other spam */ 
+#define LL_KILLEDPET  0x0100 /* Killed a tame monster */
+#define LL_DEBUG      0x8000 /* For debugging messages and other spam */
 /* Supply nethack_enter macro if not supplied by port */
 #ifndef nethack_enter
 #define nethack_enter(argc, argv) ((void) 0)
